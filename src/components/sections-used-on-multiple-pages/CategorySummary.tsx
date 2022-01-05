1f016eebd81588aa8dacc7fe220ec84602ac6793
David <>
2022-01-05 18:07:23 +0000

    basic

--- src/components/sections-used-on-multiple-pages/CategorySummary.tsx
+++ src/components/sections-used-on-multiple-pages/CategorySummary.tsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import '../../scss/category-summary.scss';
+import React, { useState, useEffect } from "react";
+import "../../scss/category-summary.scss";
+import data from "../../data.json";
+import arrow from '../../assets/shared/icon-arrow-right.svg'
+
+interface Category {
+  category: string,
+  tablet: string,
+  mobile: string, 
+  desktop: string
+}
 
 export default function CategorySummary() {
+  const [categories, setCategories] = useState<Category[] | []>([]);
 
-  
+  useEffect(() => {
+    let cats = JSON.parse(JSON.stringify(data));
+    setCategories(cats['category-images']);
+  }, []);
 
-  return (
-    <div className="homepage__category-summary">
-      <div className="home__earphones">
-      <img src={ "../../assets/category-earphones/tablet/image-yx1-earphones.jpg"} alt="" />
+  return <div className="category-summary">
+      {categories && categories.map((cat) => 
+      <>
+        <div className="category-summary__item">
+            <picture>
+            <source srcSet={cat.tablet}
+              media="(min-width: 768px)" />
+            <source srcSet={cat.desktop}
+              media="(min-width: 1024px)" />
+            <img src={`${cat.mobile}`} alt="" />
+          </picture>
+        </div>
+        <h3>{cat.category.toUpperCase()}</h3>
+        <button className="category-summary__cta">
+          <span>SHOP</span>
+          <img className="category-summary__arrow" src={arrow} alt="" />
+        </button>
+        </>
+      )}
+  </div>;
 
-      </div>
-    </div>
-  )
 }
+
