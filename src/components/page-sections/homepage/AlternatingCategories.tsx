c5306abe0c218bc3a94bbbe2b33152a89e55cb74
David <>
2022-01-07 14:45:13 +0000

    first alternating box complete

--- src/components/page-sections/homepage/AlternatingCategories.tsx
+++ src/components/page-sections/homepage/AlternatingCategories.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import '../../../scss/homepage.scss'
 
 export default function AlternatingCategories() {
   return (
-    <div>
+    <>
+    <div className="homepage__alternating-category-one">
+      <div className="homepage__alternating-category-one-copy">
+        <h3 className="homepage__alternating-category-title">ZX7 SPEAKER</h3>
+        <Link className="homepage__alternating-category-cta" to="product/zx7-speaker">SEE PRODUCT</Link>
+
+      </div>
+    </div>
+    <div className="homepage__alternating-category-two">
       
     </div>
+    </>
   )
 }
--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -4,13 +4,15 @@ import Hero from '../page-sections/homepage/Hero'
 
 import '../../scss/homepage.scss'
 import ProductHighlight from "../page-sections/homepage/ProductHighlight";
+import AlternatingCategories from "../page-sections/homepage/AlternatingCategories";
+import Description from '../page-sections/homepage/Description'
 
 export default function Homepage() {
   return <div className="homepage">
     <Hero />
     <CategorySummary />
     <ProductHighlight />
-
-
+    <AlternatingCategories />
+    <Description />
   </div>;
 }
