2f382e035dc753b957879e7a0a2b0b3e75a797fa
David <>
2022-01-18 09:44:26 +0000

    dd

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,13 @@ import "@testing-library/jest-dom";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import Enzyme from "enzyme";
 Enzyme.configure({ adapter: new Adapter() });
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
