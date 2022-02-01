711916ab97e268e2951c435be6f1d60c291bd076
Webdev-DavidM <56081000+Webdev-DavidM@users.noreply.github.com>
2022-02-01 11:36:52 +0000

    carousel changed

--- src/components/sections-used-on-multiple-pages/Carousel.js
+++ src/components/sections-used-on-multiple-pages/Carousel.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Slider from "react-slick";
-import { Link } from "react-router-dom";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from 'react';
+import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function SimpleSlider({ products }) {
   var settings = {
-    className: "center",
+    className: 'center',
     centerMode: true,
     infinite: true,
-    centerPadding: "30px",
+    centerPadding: '30px',
     slidesToShow: 3,
     speed: 500,
     arrows: true,
@@ -25,7 +25,7 @@ export default function SimpleSlider({ products }) {
         breakpoint: 678,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
@@ -35,17 +35,16 @@ export default function SimpleSlider({ products }) {
     <Slider {...settings}>
       {products &&
         products.map((product, index) => (
-          <div key={index} className="product__carousel-image-container">
-            <img className="product__carousel-image" src={product.image} />
+          <div key={index} className='product__carousel-image-container'>
+            <img className='product__carousel-image' src={product.image} />
 
-            <div className="product__title product__title--margin-top-and-bottom">
+            <div className='product__title product__title--margin-top-and-bottom'>
               {product.name}
             </div>
             <Link
               to={`/product/${product.slug}`}
-              data-test="product-cta"
-              className="product__product-cta product__product-cta--margin-top-and-bottom"
-            >
+              data-test='product-cta'
+              className='product__product-cta product__product-cta--margin-top-and-bottom'>
               SEE PRODUCT
             </Link>
           </div>
