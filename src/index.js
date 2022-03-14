69deedb9ff8ab69aac5c600db44baaae8bd084f2
David <>
2022-03-14 18:34:46 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ScrollToTop from './scrollToTop';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://audio-first.herokuapp.com/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
