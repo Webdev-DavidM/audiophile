5f09ad034144f5013b7331820db90a0e644fd231
David <>
2022-03-15 13:06:21 +0000

    s

--- src/App.tsx
+++ src/App.tsx
@@ -66,19 +66,19 @@ function App() {
       </div>}
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/audiophile/checkout" element={<PrivateRoute loggedIn={isLoggedIn}><Checkout /></PrivateRoute>} />
+        <Route path="/checkout" element={<PrivateRoute loggedIn={isLoggedIn}><Checkout /></PrivateRoute>} />
           <Route
-          path="/audiophile/category/:category"
+          path="/category/:category"
           element={<Category />}
         />
          <Route
-         path="/audiophile/product/:slug"
+         path="/product/:slug"
          element={<Product />}
        />
-        <Route path="/audiophile/login" element={<Login />} />
-        <Route path="/audiophile/sign-up" element={<SignUp />} />
-        <Route path="/audiophile/checkout" element={<Checkout />} />
-        <Route path="/audiophile/*" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/*" element={<Homepage />} />
       </Routes>
       <Footer />
       
