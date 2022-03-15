943f04f1b65e6adde5c6eb7341f3403292018611
David <>
2022-03-15 09:23:33 +0000

    deployaa

--- src/components/page-sections/homepage/Hero.tsx
+++ src/components/page-sections/homepage/Hero.tsx
@@ -14,17 +14,17 @@ export default function Hero() {
           />
           <source
             className="homepage__image"
-            srcSet="https://webdev-davidm.github.io/audiophile/assets/home/desktop/image-hero.jpg/assets/home/mobile/image-header.jpg"
+            srcSet="https://webdev-davidm.github.io/audiophile/assets/home/mobile/image-header.jpg"
             media="(max-width: 767px)"
           />
           <source
             className="homepage__image"
-            srcSet="https://webdev-davidm.github.io/audiophile/assets/home/desktop/image-hero.jpg/assets/home/tablet/image-header.jpg"
+            srcSet="https://webdev-davidm.github.io/audiophile/assets/home/tablet/image-header.jpg"
             media="(min-width: 768px, max-width: 1023px)"
           />
           <img
             className="homepage__image"
-            src="https://webdev-davidm.github.io/audiophile/assets/home/desktop/image-hero.jpg/assets/home/mobile/image-header.jpg"
+            src="https://webdev-davidm.github.io/audiophile/assets/home/mobile/image-header.jpg"
             alt="hero product"
           />
         </picture>
@@ -47,42 +47,3 @@ export default function Hero() {
     </div>
   );
 }
-
-// interface Category {
-//   category: string,
-//   tablet: string,
-//   mobile: string,
-//   desktop: string
-// }
-
-// FOR REFERENCE WHEN USING DIFFERENT IMAGE SIZES
-
-// export default function CategorySummary() {
-//   const [categories, setCategories] = useState<Category[] | []>([]);
-
-//   useEffect(() => {
-//     let cats = JSON.parse(JSON.stringify(data));
-//     setCategories(cats['category-images']);
-//   }, []);
-
-//   return <div className="category-summary">
-//       {categories && categories.map((cat) =>
-//       <>
-//         <div className="category-summary__item">
-//   <picture className="category-summary__image">
-//   <source className="category-summary__image" srcSet={cat.tablet}
-//     media="(min-width: 768px)" />
-//   <source className="category-summary__image" srcSet={cat.desktop}
-//     media="(min-width: 1024px)" />
-//   <img className="category-summary__image" src={`${cat.mobile}`} alt="" />
-// </picture>
-//           <h6 className="category-summary__category-name">{cat.category.toUpperCase()}</h6>
-//           <button className="category-summary__cta">
-//             <span className="category-summary__cta-text">SHOP</span>
-//             <img className="category-summary__arrow" src={arrow} alt="" />
-//           </button>
-//         </div>
-
-//         </>
-//       )}
-//   </div>;
